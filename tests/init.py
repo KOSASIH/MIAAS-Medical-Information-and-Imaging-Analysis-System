61a7b2e89669ae423da8c6375f40939939ff3f62
KOSASIH <kosasihg88@gmail.com>
2024-05-25 21:36:46 +0700

    Create init.py

---
+++ tests/init.py
@@ -0,0 +1,7 @@
+import unittest
+
+from .test_patient_api import PatientAPITestCase
+from .test_doctor_api import DoctorAPITestCase
+from .test_hospital_api import HospitalAPITestCase
+
+__all__ = ["PatientAPITestCase", "DoctorAPITestCase", "HospitalAPITestCase"]

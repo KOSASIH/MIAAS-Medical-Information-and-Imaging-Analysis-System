3cfb0429d1485d8f9c83ceaa660ee0cc9b1b4632
KOSASIH <kosasihg88@gmail.com>
2024-05-25 21:33:30 +0700

    Create data_loader.py

---
+++ src/utils/data_loader.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+class DataLoader:
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+
+    def load_csv(self) -> pd.DataFrame:
+        return pd.read_csv(self.data_path)
+
+    def load_numpy(self) -> np.ndarray:
+        return np.load(self.data_path)
+
+    def load_pandas(self) -> pd.DataFrame:
+        return pd.read_pickle(self.data_path)
+
+def load_data(data_path: str, data_type: str) -> object:
+    loader = DataLoader(data_path)
+    if data_type == "csv":
+        return loader.load_csv()
+    elif data_type == "numpy":
+        return loader.load_numpy()
+    elif data_type == "pandas":
+        return loader.load_pandas()
+    else:
+        raise ValueError("Invalid data type")
